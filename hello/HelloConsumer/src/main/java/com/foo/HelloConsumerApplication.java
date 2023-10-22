5bb30ce59b6abe06850ebf9c255942cd3b8334e4
jbprek <jbprek@gmail.com>
2023-10-22 12:22:11 +0100

    WIP

--- hello/HelloConsumer/src/main/java/com/foo/HelloConsumerApplication.java
+++ hello/HelloConsumer/src/main/java/com/foo/HelloConsumerApplication.java
@@ -1,6 +1,8 @@
 package com.foo;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +12,18 @@ import org.springframework.kafka.annotation.KafkaListener;
 @Slf4j
 public class HelloConsumerApplication {
 
-    @KafkaListener(id = "myId", topics = "test-topic")
+
+    @Value("#{'${spring.kafka.topics}'.split(',')}")
+    private String[] topics;
+
+    @KafkaListener(id="myId", topics = "#{'${spring.kafka.topics}'.split(',')}")
     public void listen(String in) {
        log.info(in);
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        log.info(topics.toString());
     }
 
     public static void main(String[] args) {
