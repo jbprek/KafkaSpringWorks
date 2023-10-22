b28d4ccf07b03f1a1c38612be6f36e284aefaaed
jbprek <jbprek@gmail.com>
2023-10-22 12:42:15 +0100

    WIP

--- hello/HelloConsumer/src/main/java/com/foo/HelloConsumerApplication.java
+++ hello/HelloConsumer/src/main/java/com/foo/HelloConsumerApplication.java
@@ -7,24 +7,27 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import java.util.UUID;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @Slf4j
 public class HelloConsumerApplication {
 
 
-    @Value("#{'${spring.kafka.topics}'.split(',')}")
-    private String[] topics;
+//    @Value("#{'${spring.kafka.topics}'.split(',')}")
+//    private String[] topics;
 
-    @KafkaListener(id="myId", topics = "#{'${spring.kafka.topics}'.split(',')}")
+    @KafkaListener(topics = "#{'${spring.kafka.topics}'.split(',')}")
     public void listen(String in) {
-       log.info(in);
+       log.info("### {}",in);
     }
 
-    @PostConstruct
-    public void postConstruct() {
-        log.info(topics.toString());
-    }
+//    @PostConstruct
+//    public void postConstruct() {
+//        log.info("!!! {}", Arrays.asList(topics).toString());
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloConsumerApplication.class, args);
