11381f2f0bbbe643da65961ef4fcd7e26b94d7da
jbprek <jbprek@gmail.com>
2023-10-26 19:38:14 +0100

    WIP

---
+++ confluent-tutorials/spring-confluent-producer/src/main/java/io/confluent/spring/tutorial/SpringConfluentApplication.java
@@ -0,0 +1,43 @@
+package io.confluent.spring.tutorial;
+
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+@SpringBootApplication
+@EnableKafka
+//@Configuration
+public class SpringConfluentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfluentApplication.class, args);
+    }
+
+
+    @Bean
+    public ProducerFactory<Integer, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(
+                Map.of(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                        RETRIES_CONFIG, 0,
+                        BUFFER_MEMORY_CONFIG, 33554432,
+                        KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class,
+                        VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
+                ));
+    }
+
+    @Bean
+    public KafkaTemplate<Integer, String> kafkaTemplate(ProducerFactory<Integer, String> producerFactory) {
+        return new KafkaTemplate<Integer, String>(producerFactory);
+    }
+
+}
